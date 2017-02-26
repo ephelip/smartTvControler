1926aed30f25ab2b8db42acb73ab2bd470405e9a
Phelip Etienne <etienne.phelip@savoirfairelinux.com>
2017-02-25 20:12:51 -0500

    find tvs with zeroconf
    We now use npm package bonjour to watch for registered tv on the network.

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,20 @@
 
 // Dependencies
 const routes = require('express').Router();
+const bonjour = require('bonjour')();
+const Promise = require('bluebird');
+const _ = require('lodash');
 
 // Utils
 const readJsonFile = require('../lib/utils').readJsonFile;
 const writeJsonFile = require('../lib/utils').writeJsonFile;
+const setTvToAutoZap = require('../lib/controllers').setTvToAutoZap
+const setTvToFixedChannel = require('../lib/controllers').setTvToFixedChannel
+const setModeToAllTv = require('../lib/controllers').setModeToAllTv
 
-routes.get('/channels', function(req, res) {
+const activeServices = [];
+
+routes.get('/channels', (req, res) => {
   console.log("GET /channels");
   return readJsonFile('channels.json')
     .then((channels) => {
@@ -18,7 +26,7 @@ routes.get('/channels', function(req, res) {
     });
 });
 
-routes.post('/channels', function(req, res) {
+routes.post('/channels', (req, res) => {
   if (!req.body.url || !req.body.description){
     return res.status(400).send({error:" you need url and description"});
   }
@@ -36,8 +44,8 @@ routes.post('/channels', function(req, res) {
           newChannelNumber = channel.channel + 1;
         }
       })
-      channels.channel = newChannelNumber;
-      jsonReturned.push(newChannel);
+      newChannel.channel = newChannelNumber;
+      channels.push(newChannel);
       return channels;
     })
     .then((updatedChannels) => {
@@ -52,63 +60,29 @@ routes.post('/channels', function(req, res) {
     });
 });
 
-routes.get('/tvs', function(req, res) {
+routes.get('/tvs', (req, res) => {
   console.log('GET /tvs');
-  return readJsonFile("./displays.json")
-    .then((tvs) => {
-    	return res.status(200).send(tvs);
-    })
-    .catch((err) => {
-    	console.log(err);
-    	return res.status(500).send('Internal Server Error');
+  let tvs = activeServices.reduce((tvs, tv) => {
+    tvs.push({
+      name: tv.name,
+      ip: tv.ip,
+      port: tv.port
     });
+    return tvs;
+
+
+  }, [])
+	return res.status(200).send(tvs);
 });
 
-routes.post('/tvs', function(req, res) {
-  console.log('POST /tvs');
-  if (!req.body.name || !req.body.url || !req.body.port) {
-    return res.status(400).send(`Bad Request: url, port and name required`);
-  }
-
-  let newtv = {
-    name:req.body.name,
-    url: req.body.url,
-    port:req.body.port
-  };
-
-  return readJsonFile('./displays.json')
-    .then((tvs) => {
-      let tvId = 0;
-
-      tvs.map((tv) => {
-        if(tv.id >= tvId) {
-          tvId = tv.id + 1;
-        }
-      });
-
-	    newtv.id = tvId;
-	    tvs.push(newtv);
-
-    	return writeJsonFile('./displays.json', tvs);
-    })
-    .then((done) => {
-      return res.status(200).send(newtv);
-    })
-    .catch((err) => {
-      console.log(err);
-      return res.status(500).send('Internal Server Error');
-    });
-});
-
-
-routes.post('/zap', function(req, res) {
-  console.log('Channel zap requested');
-  if ( !req.body.channel ) {
-    return res.status(400).send(`Bad Request: channel required}`);
+routes.post('/zap', (req, res) => {
+  console.log('POST /zap');
+  if (!req.body.channel) {
+    return res.status(400).send(`Bad Request: channel required`);
   }
 
   return readJsonFile('channels.json')
-    .then((channels)  => {
+    .then((channels) => {
       let channelUrl;
       channels.map((channel) => {
         if(channel.channel == req.body.channel){
@@ -118,25 +92,160 @@ routes.post('/zap', function(req, res) {
       return channelUrl;
     })
     .then((channelUrl) => {
-      if(!channelUrl) {
-        console.log('error missing channel, cannot zap');
+      if (!channelUrl) {
+        console.log('error channel not found, cannot zap');
         return res.status(400).send(`Bad Request: channel not found`);
       }
-      readJsonFile('displays.json')
-      .then((TVs) => {
-        // let promiseArrayForTVs = [];
-        TVs.map((TV) => {
-          console.log(`setting ${TV.name} to ${channelUrl}`);
-          changeChannelOnTv(TV.port, TV.url, channelUrl);
-          clearInterval(autoZapTimer);
-          autoZapTimer = setInterval(autoZapCallback,10000);
 
-          return res.status(200).send(`Channel changed}`);
-          // promiseArrayForTVs.push(changeChannelOnTv(TV.port, TV.url, channelUrl))
-        })
-        // return Promise.all(promiseArrayForTVs);
-      })
+      // Requested zap on one tv
+      if (req.body.tv) {
+        let tv = findActiveService(activeServices, req.body.tv);
+        if (!tv) {
+          console.log('tv not found');
+          return res.status(400).send(`Bad Request: tv not found`);
+        }
+
+        return setTvToFixedChannel(tv, channelUrl);
+      }
+
+      // No tv requested, setting channel to all TVs
+      return setModeToAllTv(activeServices, setTvToFixedChannel(tv, channelUrl));
+    })
+    .then((done) => {
+      return res.status(200).send(`Channel changed`);
+    })
+    .catch((err) => {
+      console.log(err);
+      return res.status(500).send(`Internal Server Error`);
     });
+});
+
+routes.post('/autozap', (req, res) => {
+  console.log('POST /autoZap');
+  if (!req.body.channel) {
+    return res.status(400).send(`Bad Request: channel required`);
+  }
+
+  // Default reload time set to 10 seconds
+  let reloadTime = 10000;
+  if (Number.isInteger(req.body.reloadTime)) {
+    reloadTime = req.body.reloadTime * 1000; // convert to ms
+  }
+  return Promise.resolve()
+    .then(() => {
+      // Requested auto zap on one tv
+      if (req.body.tv) {
+        let tv = findActiveService(activeServices, req.body.tv);
+        if (!tv) {
+          console.log('tv not found');
+          return res.status(400).send(`Bad Request: tv not found`);
+        }
+
+        return setTvToAutoZap(tv, timer);
+      }
+
+      // No tv requested, setting channel to all TVs
+      return setModeToAllTv(activeServices, setTvToAutoZap(tv, timer));
+    })
+    .then((done) => {
+      return res.status(200).send(`Channel changed`);
+    })
+    .catch((err) => {
+      console.log(err);
+      return res.status(500).send(`Internal Server Error`);
+    });
+});
+
+
+/*******************************************************************************
+                          Watch and handle tvs
+*******************************************************************************/
+
+// browse for all http services
+var browser = bonjour.find({ type: 'http' }, (service) => {
+});
+
+browser.on('up', (service) => {
+  console.log(`+ New service found ${service.name}`);
+  activeServices.push({
+    name: service.name,
+    ip: service.referer.address,
+    port: service.referer.port
+  });
+
+  // Set all new tvs to autozap with 10 seconds reload
+  let tv = findActiveService(activeServices, service.name);
+  return setTvToAutoZap(tv, 10000);
+});
+
+browser.on('down', (service) => {
+  console.log(`- Service ${service.name} has stopped`);
+  removeActiveService(activeServices, service.name);
 })
+
+function removeActiveService(services, name) {
+  let serviceIndex = _.findIndex(services, (s) => { return s.name === name; });
+
+  if (serviceIndex > -1) {
+    if (services[serviceIndex].autoZapTimer) {
+      clearInterval(services[serviceIndex].autoZapTimer);
+      delete services[serviceIndex].autoZapTimer;
+    }
+    services.splice(serviceIndex, 1);
+  }
+}
+
+function findActiveService(services, name) {
+  let serviceIndex = _.findIndex(services, (s) => { return s.name === name; });
+  if (serviceIndex > -1) {
+    return services[serviceIndex];
+  }
+  return false;
+}
+/******************************************************************************/
+
+
+
+/*******************************************************************************
+                           FOR TESTING
+
+                  Simulate service creation and deletion
+*******************************************************************************/
+
+function pubService(name){
+  bonjour.publish({ name: name, type: 'http', port: 3000 });
+}
+
+function killService(service){
+  service.stop();
+}
+
+function countServices(services){
+  console.log(`There is ${services.length} active services`);
+}
+
+setTimeout(() => {
+  pubService('TV accueil');
+  countServices(activeServices);
+}, 2000);
+
+let serviceToKill;
+setTimeout(() => {
+  serviceToKill = bonjour.publish({ name: 'killable service', type: 'http', port: 3000 });
+  pubService('TV 3eme');
+  countServices(activeServices);
+}, 5000);
+
+setTimeout(() => {
+  killService(serviceToKill);
+  countServices(activeServices);
+}, 8000);
+
+setTimeout(() => {
+  pubService('TV default');
+  countServices(activeServices);
+}, 10000);
+
+/******************************************************************************/
 
 module.exports = routes;
